85d4c32641c951a69a60d526cd8c19164ca97804
GlaucoInfinities <162525681+GlaucoInfinities@users.noreply.github.com>
2025-07-24 15:05:44 +1000

    login by email

--- app/api/auth.py
+++ app/api/auth.py
@@ -28,12 +28,12 @@ async def register(data: RegisterRequest, db: AsyncSession = Depends(get_db)):
 
 @router.post("/login")
 async def login(data: LoginRequest, db: AsyncSession = Depends(get_db)):
-    username = data.username
+    email = data.email
     password = data.password
 
-    result = await db.execute(select(User).where(User.username == username))
+    result = await db.execute(select(User).where(User.email == email))
     user = result.scalar()
     if not user or not pwd_context.verify(password, user.password_hash):
         raise HTTPException(status_code=400, detail="Invalid credentials")
     token = jwt.encode({"sub": str(user.id)}, SECRET_KEY, algorithm=ALGORITHM)
-    return {"access_token": token,"username": user.username, "user_id": str(user.id)}
+    return {"access_token": token,"username": user.email, "user_id": str(user.id)}
