f9f317fa7e1da834f9ed443a5be26f99618f89aa
GlaucoInfinities <162525681+GlaucoInfinities@users.noreply.github.com>
2025-08-23 10:40:49 +0900

    config webhook

--- app/core/config.py
+++ app/core/config.py
@@ -30,6 +30,8 @@ class Settings(BaseSettings):
     AWS_ACCESS_KEY_ID: str
     AWS_SECRET_ACCESS_KEY: str
 
+    PUBLIC_BASE_URL: str
+
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8",extra='ignore' )
 
 settings = Settings()
