92a9740bca2014d7d3c348b283a84e5ac13d748c
GlaucoInfinities <162525681+GlaucoInfinities@users.noreply.github.com>
2025-12-03 09:10:25 +1000

    all origins

--- app/main.py
+++ app/main.py
@@ -38,14 +38,23 @@ origins = [
 ]
 
 app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  # tighten for prod
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
+'''
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+'''
 app.include_router(auth_router)
 app.include_router(router)
 app.include_router(push_router)
