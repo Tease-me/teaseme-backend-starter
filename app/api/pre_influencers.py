a8ab46633ae6481530164c16c893200c5d640ee5
GlaucoInfinities <162525681+GlaucoInfinities@users.noreply.github.com>
2025-12-21 23:52:56 +1000

    Update pre_influencers.py

--- app/api/pre_influencers.py
+++ app/api/pre_influencers.py
@@ -237,7 +237,7 @@ async def register_pre_influencer(
     try:
         #tid = request.cookies.get("_fprom_tid")
         tid = data.fp_tid or request.cookies.get("_fprom_tid")
-        log.info("FP TID =", tid)
+        log.info("FP TID=%s", tid)
 
         res = await fp_track_signup(
             email=pre.email,
