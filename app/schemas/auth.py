cba3be6c875e69e9b45584f4f9af9fa4140becd3
Ez <75935138+EzAuto399@users.noreply.github.com>
2026-02-02 09:53:23 +1000

    added 3 input

--- app/schemas/auth.py
+++ app/schemas/auth.py
@@ -8,7 +8,10 @@ class RegisterRequest(BaseModel):
     password: str
     email: str
     influencer_id: str | None = None
-
+    full_name: str | None = None
+    gender: str | None = None
+    date_of_birth: str | None = None
+    
 class Token(BaseModel):
     access_token: str
     refresh_token: str
